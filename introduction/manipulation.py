a92a0e3bf97c23c209fe100c97169eca19482839
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-01 20:44:27 +0530

    Initial commit

---
+++ introduction/manipulation.py
@@ -0,0 +1,4 @@
+# this code is for adding multiple code by using one print() function and also how to change the line of the string
+# by \n
+print("Hello world! "+" Vedant"+"\n")
+print("Hello world!\n"+"Hello world!\n"+"Hello world!")
