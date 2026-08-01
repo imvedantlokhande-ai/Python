527910515223d9ab424914dd9d0341d99c99fcac
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-01 22:41:14 +0530

    Initial commit

---
+++ introduction/mathematical_operators.py
@@ -0,0 +1,13 @@
+#Learn to use the basic mathematical operators, +, -, *, /, // and **
+print("My age: " + str(18))
+print(123 + 456)
+print(456 - 123)
+print(3 *2)
+print(5 / 2)
+print(5 // 2)
+print(3 ** 2)
+# the operations involve BODMAS Rule
+#What is the output of this code?
+print(3 * 3 + 3 / 3 - 3)
+#Change the code so it outputs 3?
+print(3 * (3 + 3) / 3 - 3)
