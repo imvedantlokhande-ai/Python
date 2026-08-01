70ef49edffbc82b3299f5481b70282fa8b201dcc
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-01 22:24:32 +0530

    Initial commit

---
+++ introduction/types_of_Data_Types.py
@@ -0,0 +1,19 @@
+# types of Data Types
+#Subscription
+print("Hello"[4])
+
+#String
+print("123"+"456")
+
+#integer = Whole number
+print(123+456)
+
+#Large Integer
+print(123_456_789)
+
+#Float =Floating Point number
+print(3.1456788)
+
+#Bloolean
+print(True)
+print(False)
