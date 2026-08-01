6598069d85f280253c7b2b19091dfbf68d959e2f
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-01 22:09:22 +0530

    Remove brand_name_generator_project.py

--- introduction/brand_name_generator_project.py
+++
@@ -1,5 +0,0 @@
-# A code made by me to make a creative brand name by use of functions(print,input) and try to use variable in it
-print("Welcome to the brand name generator ")
-city = input("What's the name of the city you grew up in ?\n")
-pet = input("what's your pet name?\n")
-print("Your brand name could be "+city+" "+pet)
