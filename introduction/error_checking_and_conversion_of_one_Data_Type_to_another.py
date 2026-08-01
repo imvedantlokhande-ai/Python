2867eca8816600b255807c35a0d36dc4a8652e8b
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-01 22:28:28 +0530

    Initial commit

---
+++ introduction/error_checking_and_conversion_of_one_Data_Type_to_another.py
@@ -0,0 +1,11 @@
+# error checking and conversion pf one Data Type to another
+len("Hello")
+
+print(type("abcd"))
+print(type(123))
+print(type(123.456))
+print(type(True))
+
+name = input("Enter your name")
+length_of_name = len(name)
+print("Number of letters in your name: " + str(length_of_name))
