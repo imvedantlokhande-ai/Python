3d6c648af31b43aa44e7900f31e72e55336a9470
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-02 13:00:20 +0530

    Initial commit

---
+++ conditional loops/learnings/multiple_ifs.py
@@ -0,0 +1,22 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        bill=5
+        print("Please pay $5.")
+    elif age <= 18:
+        bill=7
+        print("Please pay $7.")
+    else:
+        bill=12
+        print("Please pay $12.")
+    photo=input("want photo write y or not then n")
+    if photo=="y":
+        bill += 3
+
+    print(f"your final bill is: {bill}")
+else:
+    print("Sorry you have to grow taller before you can ride.")
