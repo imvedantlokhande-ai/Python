9b7bfc84d0270e314b8dcbabca3f561071e099fe
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-03 20:33:23 +0530

    Initial commit

---
+++ functions/learning/functions_with_input.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("Hello World")
+#     print("How are you?")
+#     print("it's good see you!")
+#
+# greet()
+# greet()
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"How do you do {name}?")
+
+greet_with_name("Vedant")
