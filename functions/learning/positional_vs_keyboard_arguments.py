4544efb859c95df7f05ee0497810720417ade06d
Vedant Lokhande <imvedantlokhande@gmail.com>
2026-08-03 21:04:02 +0530

    Initial commit

---
+++ functions/learning/positional_vs_keyboard_arguments.py
@@ -0,0 +1,12 @@
+
+# Functions with input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Jack Bauer")
+def greet(name, greeting):
+    print(f"{greeting} {name}")
+greet("Angela", "Yo!")
